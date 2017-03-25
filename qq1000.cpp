43ce0b3d5b8c953e8137fa9226fbe13039e4abbc
Ziseon Jiao <ziseon.jiao@sap.com>
2017-03-25 17:16:42 +0800

    Fix conquer sort bug with wrong cmp

--- qq1000.cpp
+++ qq1000.cpp
@@ -20,12 +20,17 @@ template <typename... Vars>
 inline void time_end(const clock_t begin, const char* message, Vars&&... args) {
   printf("Task: ");
   printf(message, args...);
-  printf(" -- %.2fs\n", double(std::clock() - begin) / CLOCKS_PER_SEC);
+  printf("\t--%.2fs\n", double(std::clock() - begin) / CLOCKS_PER_SEC);
 }
 
-void create_random_qq_datafile(const char* file, const int n) {
+template <>
+inline void time_end(const clock_t begin, const char* message) {
+  time_end(begin, "%s", message);
+}
+
+void create_random_qq_datafile(const char* file, const int n,
+                               const int max_qq_number) {
   auto begin = time_begin();
-  const int kMaxQQNumber = 100000;
   const int kThreadCount = 2;
   std::ofstream out(file);
   std::vector<std::thread> threads;
@@ -35,7 +40,7 @@ void create_random_qq_datafile(const char* file, const int n) {
       char buffer[16];
       std::random_device rd;
       std::mt19937 gen(rd());
-      std::uniform_int_distribution<> dis(1, kMaxQQNumber);
+      std::uniform_int_distribution<> dis(1, max_qq_number);
       for (int i = 0; i < n / kThreadCount; i++) {
         snprintf(buffer, 16, "%d\n", dis(gen));
         out << buffer;
@@ -56,29 +61,26 @@ void create_random_qq_datafile(const char* file, const int n) {
   time_end(begin, "Create random QQ %d", n);
 }
 
+using QQFrequencyDict = std::unordered_map<uint64_t, uint64_t>;
+using QQFrequency = std::pair<uint64_t, uint64_t>;
+
 template <typename CountsHandler>
 void count_qq_partially(const char* file, CountsHandler& handler) {
-  const int kMaxMapSize = 10000;
+  auto begin = time_begin();
+
   std::ifstream in(file);
   uint64_t qq;
-  std::unordered_map<uint64_t, uint64_t> qq_count_map;
   while (in >> qq) {
-    qq_count_map[qq]++;
-    if (qq_count_map.size() > kMaxMapSize) {
-      // dump data to handler
-      for (auto& pair : qq_count_map) {
-        handler.handle(pair.first, pair.second);
-      }
-      qq_count_map.clear();
-    }
+    // dump data to handler
+    handler.handle(qq);
   }
-  for (auto& pair : qq_count_map) {
-    handler.handle(pair.first, pair.second);
-  }
+  handler.flush();
+
+  time_end(begin, "Counting Phase 1");
 }
 
 class QQStorageManager {
-  int _n;
+  const int _n;
 
  public:
   explicit QQStorageManager(int n) : _n(n) {}
@@ -93,16 +95,25 @@ class QQStorageManager {
 class QQStorage {
   std::vector<std::ofstream> _outs;
   QQStorageManager _manager;
+  int _times;
 
  public:
-  QQStorage(const QQStorageManager& manager) : _manager(manager) {
+  QQStorage(const QQStorageManager& manager) : _manager(manager), _times(0) {
     for (int i = 0; i < _manager.size(); i++) {
       _outs.emplace_back(_manager.getStorageName(i));
     }
   }
 
-  void storage(uint64_t qq, uint64_t count) {
-    _outs[qq % _outs.size()] << qq << " " << count << "\n";
+  inline int size() const { return _manager.size(); }
+
+  void storage(const std::vector<QQFrequencyDict>& dicts) {
+    auto dump_begin = time_begin();
+    for (int i = 0; i < _manager.size(); i++) {
+      for (const auto& pair : dicts[i]) {
+        _outs[i] << pair.first << " " << pair.second << "\n";
+      }
+    }
+    time_end(dump_begin, "Dump %d", ++_times);
   }
 
  private:
@@ -112,11 +123,11 @@ class QQStorage {
 
 class QQFrequencyDataReducer {
   std::vector<std::ifstream> _ins;
-  QQStorageManager _manager;
+  const QQStorageManager _manager;
   const int _top;
 
  public:
-  using TopFrequencyList = std::vector<std::pair<uint64_t, uint64_t>>;
+  using TopFrequencyList = std::vector<QQFrequency>;
   using TopIterator = TopFrequencyList::iterator;
 
   QQFrequencyDataReducer(const QQStorageManager& manager, int top)
@@ -127,8 +138,10 @@ class QQFrequencyDataReducer {
   }
 
   TopFrequencyList reduce() {
+    auto begin = time_begin();
     std::vector<TopFrequencyList> results;
-    // file[] : (count => qq)
+
+    // frequency => qq
     for (int i = 0; i < _manager.size(); i++) {
       results.push_back(reduce(i));
     }
@@ -143,29 +156,36 @@ class QQFrequencyDataReducer {
       }
     }
 
-    auto topCmp = [](topItr o1, topItr o2) {
-      return o2.first->first < o1.first->first;
+    auto topCmp = [](const topItr& o1, const topItr& o2) {
+      return (o1.first)->first < (o2.first)->first;
     };
     std::make_heap(top_heap.begin(), top_heap.end(), topCmp);
+    for (const auto& top : top_heap) {
+      std::cout << (top.first)->first << " " << (top.first)->second
+                << std::endl;
+    }
 
     // find Top K !
     int i = 0;
     while ((!top_heap.empty()) && i < _top) {
+      std::pop_heap(top_heap.begin(), top_heap.end(), topCmp);
       result.push_back(*(top_heap.back().first));
-      top_heap.back().first++;
+      (top_heap.back().first)++;
       if (top_heap.back().first == top_heap.back().second) {
-        std::pop_heap(top_heap.begin(), top_heap.end(), topCmp);
         top_heap.pop_back();
       } else {
         std::push_heap(top_heap.begin(), top_heap.end(), topCmp);
       }
       i++;
     }
+    time_end(begin, "Reduce");
     return result;
   }
 
   TopFrequencyList reduce(int i) {
     std::ifstream& in = _ins[i];
+
+    // frequency => qq
     std::unordered_map<uint64_t, uint64_t> frequent_map;
     uint64_t qq;
     uint64_t count;
@@ -183,7 +203,7 @@ class QQFrequencyDataReducer {
     }
     std::cout << "Top " << _top << " QQ in file" << _manager.getStorageName(i)
               << std::endl;
-    for (auto& topQQ : result) {
+    for (const auto& topQQ : result) {
       std::cout << topQQ.second << ": " << topQQ.first << std::endl;
     }
     return result;
@@ -196,28 +216,49 @@ class QQFrequencyDataReducer {
 
 class DumpQQCountsHandler {
   QQStorage _storage;
+  std::vector<QQFrequencyDict> _groupQQFrequencyDicts;
+  int _size;
+  const int kMaxSize = 1 << 16;
 
  public:
   explicit DumpQQCountsHandler(const QQStorageManager& storageManager)
-      : _storage(storageManager) {}
+      : _storage(storageManager),
+        _groupQQFrequencyDicts(_storage.size()),
+        _size(0) {}
+
   ~DumpQQCountsHandler() { std::cout << "close.." << std::endl; }
 
-  inline void handle(uint64_t qq, uint64_t count) {
-    _storage.storage(qq, count);
+  inline void handle(const uint64_t qq) {
+    if (_groupQQFrequencyDicts[qq % _storage.size()][qq]++ == 0) {
+      if (++_size > kMaxSize) {
+        dump();
+      }
+    }
   }
 
+  inline void flush() { dump(); }
+
  private:
+  inline void dump() {
+    _storage.storage(_groupQQFrequencyDicts);
+    for (auto& dict : _groupQQFrequencyDicts) {
+      dict.clear();
+    }
+    _size = 0;
+  }
+
   DumpQQCountsHandler(const DumpQQCountsHandler&);
   DumpQQCountsHandler& operator=(const DumpQQCountsHandler&);
 };
 
 int main(int argc, char** argv) {
   const char* qq_file_name = "qq.txt";
-  const int qq_num = 10000000;
+  const int kQQNum = 10000000;
+  const uint64_t kMaxQQNumber = 100000;
   const int top = 10;
 
   // 1. create random qq number.
-  create_random_qq_datafile(qq_file_name, qq_num);
+  create_random_qq_datafile(qq_file_name, kQQNum, kMaxQQNumber);
 
   // 2. counting and storage
   QQStorageManager storageManager(10);
